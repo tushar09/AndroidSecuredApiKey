9190c245973ab1c87efc58d0a043f6c3aeec407c
Tushar Monirul <wtushar09@gmail.com>
2021-11-09 22:51:34 +0600

    installed encrypted sharedpreferences

--- app/src/main/java/com/captaindroid/secureapi/MainActivity.java
+++ app/src/main/java/com/captaindroid/secureapi/MainActivity.java
@@ -1,27 +1,19 @@
 package com.captaindroid.secureapi;
 
+import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.IgnoreExtraProperties;
-import com.google.firebase.database.Logger;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private String TAG = "Main";
